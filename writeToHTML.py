242bb1a1ad9b2bfcbd530f9b2c40369ccc16ddd0
ankitakul <47610108+ankitakul@users.noreply.github.com>
2021-04-21 17:48:18 -0400

    update: added code to get data from dictionary
    This code take data from dictionary and prints to a html page.

--- writeToHTML.py
+++ writeToHTML.py
@@ -1,10 +1,28 @@
 content='''
 <html><head><title>Webpage Template</title></head>
 <body>
-Hello, {strname} !
+{strname}
+{htmlpage_data}
 </body></html>'''
 
-strname = input('enter name: ')
+htmlpage_data =''
+
+def printItems(dictObj, indent, htmlpage):
+
+    for key in dictObj:
+        htmlpage = htmlpage+ '<h2>'+ key +  '</h2>'
+        htmlpage = htmlpage +   '<ul>'
+        for k, v in dictObj[key].items() :
+            htmlpage = htmlpage +'<li>' + k + ' : ' + str(v) + '</li>'
+        htmlpage = htmlpage +'</ul>'
+    return htmlpage        
+
+
+
+name = input('enter name: ') #optional 
+strname = '<h1>' 'Hello '+ name + '!!' + '</h1>' #optional
+dict = {'category 1 : cooking':{'apple':1,'banana': 2 ,'bread':1} , 'category 2 : activity':{'yoga': 'url','park': 'url' ,'painting': 'url'} , 'category 3 : aaa':{'yoga': 'url','park': 'url' ,'painting': 'url'} }
+htmlpage_data = printItems(dict, 0, htmlpage_data)
 htmlcontent = content.format(**locals())
 
 outfile= open('template.html','w')
